b2efa02962cddaf3659f8b2a3c4853b1f9d1049b
Hamdiye Uzuner <hamdiye.uzuner@uni-due.de>
2025-09-03 20:13:35 +0300

    Deploy report

--- data/raw/2f6462b3faed224e692ba89a1b4306a5b67405ec2ea02f1f7c81754cb37ea159/predictions/plots/plot_0.js
+++
@@ -1,1 +0,0 @@
-let show_plot_0=false;let plot_0={"$schema":"https://vega.github.io/schema/vega-lite/v4.json","width":"container","layer":[{"data":{"values":null},"mark":"bar","encoding":{"x":{"field":"key","sort":{"field":"value","order":"descending"},"title":"SimulatedSample"},"y":{"field":"value","type":"quantitative","title":null}}}]}
---
+++ data/raw/2f6462b3faed224e692ba89a1b4306a5b67405ec2ea02f1f7c81754cb37ea159/similarities/functions.js
@@ -0,0 +1,18 @@
+function custom_func_132b6577a46539e7ae5ee44ca75af223(value, row) { try { 
+  return [{"diff": value, "lineage_pair": row["Lineage_Pair_1"]}]
+ } catch (e) { datavzrd.custom_error(e, 'Diff_1') }}
+function custom_func_309a704e53d8003a4beae6db6565e360(value, row) { try { 
+  return [{"diff": value, "lineage_pair": row["Lineage_Pair_3"]}]
+ } catch (e) { datavzrd.custom_error(e, 'Diff_3') }}
+function custom_func_d25f1e9317322721f4956bfa5662193b(value, row) { try { 
+  return [{"shared": value, "lineage_pair": row["Lineage_Pair_2"]}]
+ } catch (e) { datavzrd.custom_error(e, 'Shared_2') }}
+function custom_func_c72bc86811ad018ab9b93466fff06f12(value, row) { try { 
+  return [{"diff": value, "lineage_pair": row["Lineage_Pair_2"]}]
+ } catch (e) { datavzrd.custom_error(e, 'Diff_2') }}
+function custom_func_af994e3110e8eb9beaaefe4b3794ad38(value, row) { try { 
+  return [{"shared": value, "lineage_pair": row["Lineage_Pair_1"]}]
+ } catch (e) { datavzrd.custom_error(e, 'Shared_1') }}
+function custom_func_d4507c19d28771a2c502eab85404e430(value, row) { try { 
+  return [{"shared": value, "lineage_pair": row["Lineage_Pair_3"]}]
+ } catch (e) { datavzrd.custom_error(e, 'Shared_3') }}
